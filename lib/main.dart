4b3db20f95ca35897a12aaf5eb784938a380af9c
Arata <aratadayoon@yahoo.co.jp>
2024-07-25 23:42:16 +0900

    feat: オープニング

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,47 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const SplashScreen(
+        splashDuration: Duration(seconds: 2), // 表示時間を調整
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  final Duration splashDuration;
+
+  const SplashScreen({super.key, required this.splashDuration});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(widget.splashDuration, () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const MyHomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/icon-512x512.png',
+          width: 230,
+          height: 230,
+        ),
+      ),
     );
   }
 }

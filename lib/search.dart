c1dc36665b1428a30c85506ee187b3b874b3a109
Arata <aratadayoon@yahoo.co.jp>
2024-07-25 11:35:16 +0900

    add: 検索ページ

--- lib/search.dart
+++ lib/search.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'home.dart';
 
 class Search extends StatelessWidget {
   const Search({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.contains('/tag/') ||
+                request.url.contains('/search?q=')) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ArticlePage(url: request.url),
+                ),
+              );
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://web-view-blog-app.vercel.app/keyword'));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('検索'),
-        surfaceTintColor: Colors.transparent,
         backgroundColor: Colors.white,
-        elevation: 4,
+        surfaceTintColor: Colors.transparent,
+        title: const Text('キーワードで探す'),
       ),
-      body: const Center(
-        child: Text('Search Page'),
-      ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }

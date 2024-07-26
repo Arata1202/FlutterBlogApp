396ab3556e1e61fb4b4710bddb1143f3048cf0bb
Arata <aratadayoon@yahoo.co.jp>
2024-07-26 09:23:12 +0900

    fix: プライバシーページ遷移

--- lib/privacy.dart
+++ lib/privacy.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Privacy extends StatelessWidget {
   const Privacy({super.key});
@@ -7,6 +8,20 @@ class Privacy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) async {
+            if (request.url.contains('google.com')) {
+              if (await canLaunch(request.url)) {
+                await launch(request.url, forceSafariVC: false);
+                return NavigationDecision.prevent;
+              }
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
       ..loadRequest(Uri.parse('https://web-view-blog-app.netlify.app/privacy'));
     return Scaffold(
       appBar: AppBar(
@@ -16,11 +31,10 @@ class Privacy extends StatelessWidget {
           'assets/title.webp',
           height: 28,
         ),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(30.0),
-          child: Container(
-            alignment: Alignment.center,
-            child: const Text(
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(30.0),
+          child: Center(
+            child: Text(
               'プライバシーポリシー',
               style: TextStyle(
                 color: Colors.black,

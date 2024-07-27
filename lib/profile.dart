be9a6add54bfc09d86b892c55eec3b5c8b5c74aa
Arata <aratadayoon@yahoo.co.jp>
2024-07-28 00:15:12 +0900

    feat: profile 広告

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = WebViewController()
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  late WebViewController _controller;
+  BannerAd? _bannerAd;
+
+  @override
+  void initState() {
+    super.initState();
+    _createBannerAd();
+
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.transparent)
       ..loadRequest(Uri.parse('https://web-view-blog-app.netlify.app/profile'));
+  }
 
+  void _createBannerAd() {
+    String adUnitId = dotenv.get('TEST_BANNER_AD_ID_PROFILE');
+    _bannerAd = BannerAd(
+      adUnitId: adUnitId,
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {});
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          print('Ad failed to load: $error');
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -35,7 +69,20 @@ class Profile extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: WebViewWidget(controller: controller),
+      body: Column(
+        children: [
+          if (_bannerAd != null)
+            Container(
+              color: Colors.white,
+              width: _bannerAd!.size.width.toDouble(),
+              height: _bannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _bannerAd!),
+            ),
+          Expanded(
+            child: WebViewWidget(controller: _controller),
+          ),
+        ],
+      ),
     );
   }
 }

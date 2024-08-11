1c19b1421e9cbb1e1e4fca3adad4ee4ce4684c98
Arata <aratadayoon@yahoo.co.jp>
2024-08-11 22:43:24 +0900

    fix: スプラッシュ フェードアウト

--- lib/layout/splash/index.dart
+++ lib/layout/splash/index.dart
@@ -11,14 +11,20 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 1.0;
+
   @override
   void initState() {
     super.initState();
-    _navigateToHomeAsync();
+    _startFadeOut();
   }
 
-  Future<void> _navigateToHomeAsync() async {
-    await Future.delayed(widget.splashDuration);
+  Future<void> _startFadeOut() async {
+    await Future.delayed(widget.splashDuration - Duration(milliseconds: 300));
+    setState(() {
+      _opacity = 0.0;
+    });
+    await Future.delayed(Duration(milliseconds: 300));
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (context) => const MyHomePage()),
     );
@@ -28,11 +34,15 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Image.asset(
-          'assets/icon-512x512.png',
-          width: 230,
-          height: 230,
+      body: AnimatedOpacity(
+        opacity: _opacity,
+        duration: Duration(milliseconds: 300),
+        child: Center(
+          child: Image.asset(
+            'assets/icon-512x512.png',
+            width: 230,
+            height: 230,
+          ),
         ),
       ),
     );

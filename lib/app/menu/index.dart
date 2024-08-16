be8798ef9f887da1e7133004e43c985baa63b6f1
Arata <aratadayoon@yahoo.co.jp>
2024-08-16 23:52:15 +0900

    #12 add: MenuPage

--- lib/app/menu/index.dart
+++ lib/app/menu/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share_plus/share_plus.dart';
@@ -14,6 +15,10 @@ import '../../components/menu/history/index.dart';
 import '../../components/menu/favorite/index.dart';
 import '../../common/admob/banner/index.dart';
 import 'package:package_info/package_info.dart';
+import 'dart:io' show Platform;
+
+bool isAndroid = Platform.isAndroid;
+bool isIOS = Platform.isIOS;
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -42,98 +47,175 @@ class _MenuState extends State<Menu> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: _buildNavigationBar(context),
-      child: Column(
-        children: [
-          BannerAdWidget(adUnitId: dotenv.get('PRODUCTION_BANNER_AD_ID_MENU')),
-          Expanded(
-            child: Container(
-              color: CupertinoColors.systemGrey6,
-              child: CupertinoScrollbar(
-                child: ListView(
-                  children: [
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem("プッシュ通知設定", CupertinoIcons.bell,
+    if (isIOS) {
+      return CupertinoPageScaffold(
+        navigationBar: _buildNavigationBar(context),
+        child: Column(
+          children: [
+            BannerAdWidget(
+                adUnitId: dotenv.get('PRODUCTION_BANNER_AD_ID_MENU')),
+            Expanded(
+              child: Container(
+                color: CupertinoColors.systemGrey6,
+                child: CupertinoScrollbar(
+                  child: ListView(
+                    children: [
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem("プッシュ通知設定", CupertinoIcons.bell,
+                              _navigateToPushNotificationSettings),
+                        ],
+                      ),
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem(
+                              "レビューを送信", CupertinoIcons.star, _requestReview),
+                          _menuItem("アプリをシェア", CupertinoIcons.share, _shareApp),
+                        ],
+                      ),
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem("閲覧履歴", CupertinoIcons.time, () {
+                            _navigateTo(context, const History());
+                          }),
+                          _menuItem("お気に入り", CupertinoIcons.heart, () {
+                            _navigateTo(context, const Favorite());
+                          }),
+                        ],
+                      ),
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem("リアル大学生について", CupertinoIcons.person, () {
+                            _navigateTo(context, const Profile());
+                          }),
+                          _menuItem("リアル大学生のSNS", CupertinoIcons.globe, () {
+                            _navigateTo(context, const Sns());
+                          }),
+                        ],
+                      ),
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem("プライバシーポリシー", CupertinoIcons.lock, () {
+                            _navigateTo(context, const Privacy());
+                          }),
+                          _menuItem(
+                              "免責事項", CupertinoIcons.exclamationmark_circle,
+                              () {
+                            _navigateTo(context, const Disclaimer());
+                          }),
+                          _menuItem("著作権", CupertinoIcons.doc_text, () {
+                            _navigateTo(context, const Copyright());
+                          }),
+                          _menuItem("リンク", CupertinoIcons.link, () {
+                            _navigateTo(context, const Link());
+                          }),
+                        ],
+                      ),
+                      CupertinoListSection.insetGrouped(
+                        backgroundColor: CupertinoColors.systemGrey6,
+                        children: [
+                          _menuItem(
+                              "お問い合わせ", CupertinoIcons.mail, _launchMailApp),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              _appVersion,
+                              style: TextStyle(
+                                color: CupertinoColors.systemGrey,
+                                fontSize: 12,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: _buildAppBar(context),
+        body: Column(
+          children: [
+            BannerAdWidget(
+                adUnitId: dotenv.get('PRODUCTION_BANNER_AD_ID_MENU')),
+            Expanded(
+              child: Container(
+                color: Colors.grey[200],
+                child: Scrollbar(
+                  child: ListView(
+                    children: [
+                      _buildListSection([
+                        _menuItem("プッシュ通知設定", Icons.notifications,
                             _navigateToPushNotificationSettings),
-                      ],
-                    ),
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem(
-                            "レビューを送信", CupertinoIcons.star, _requestReview),
-                        _menuItem("アプリをシェア", CupertinoIcons.share, _shareApp),
-                      ],
-                    ),
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem("閲覧履歴", CupertinoIcons.time, () {
+                      ]),
+                      _buildListSection([
+                        _menuItem("レビューを送信", Icons.star, _requestReview),
+                        _menuItem("アプリをシェア", Icons.share, _shareApp),
+                      ]),
+                      _buildListSection([
+                        _menuItem("閲覧履歴", Icons.history, () {
                           _navigateTo(context, const History());
                         }),
-                        _menuItem("お気に入り", CupertinoIcons.heart, () {
+                        _menuItem("お気に入り", Icons.favorite, () {
                           _navigateTo(context, const Favorite());
                         }),
-                      ],
-                    ),
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem("リアル大学生について", CupertinoIcons.person, () {
+                      ]),
+                      _buildListSection([
+                        _menuItem("リアル大学生について", Icons.person, () {
                           _navigateTo(context, const Profile());
                         }),
-                        _menuItem("リアル大学生のSNS", CupertinoIcons.globe, () {
+                        _menuItem("リアル大学生のSNS", Icons.public, () {
                           _navigateTo(context, const Sns());
                         }),
-                      ],
-                    ),
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem("プライバシーポリシー", CupertinoIcons.lock, () {
+                      ]),
+                      _buildListSection([
+                        _menuItem("プライバシーポリシー", Icons.lock, () {
                           _navigateTo(context, const Privacy());
                         }),
-                        _menuItem("免責事項", CupertinoIcons.exclamationmark_circle,
-                            () {
+                        _menuItem("免責事項", Icons.warning, () {
                           _navigateTo(context, const Disclaimer());
                         }),
-                        _menuItem("著作権", CupertinoIcons.doc_text, () {
+                        _menuItem("著作権", Icons.description, () {
                           _navigateTo(context, const Copyright());
                         }),
-                        _menuItem("リンク", CupertinoIcons.link, () {
+                        _menuItem("リンク", Icons.link, () {
                           _navigateTo(context, const Link());
                         }),
-                      ],
-                    ),
-                    CupertinoListSection.insetGrouped(
-                      backgroundColor: CupertinoColors.systemGrey6,
-                      children: [
-                        _menuItem(
-                            "お問い合わせ", CupertinoIcons.mail, _launchMailApp),
+                      ]),
+                      _buildListSection([
+                        _menuItem("お問い合わせ", Icons.mail, _launchMailApp),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Text(
                             _appVersion,
-                            style: TextStyle(
-                              color: CupertinoColors.systemGrey,
+                            style: const TextStyle(
+                              color: Colors.grey,
                               fontSize: 12,
                             ),
                             textAlign: TextAlign.center,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ]),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
   }
 
   CupertinoNavigationBar _buildNavigationBar(BuildContext context) {
@@ -146,12 +228,47 @@ class _MenuState extends State<Menu> {
     );
   }
 
-  CupertinoListTile _menuItem(String title, IconData icon, VoidCallback onTap) {
-    return CupertinoListTile(
-      title: Text(title),
-      leading: Icon(icon),
-      trailing: const CupertinoListTileChevron(),
-      onTap: onTap,
+  Widget _menuItem(String title, IconData icon, VoidCallback onTap) {
+    if (isIOS) {
+      return CupertinoListTile(
+        title: Text(title),
+        leading: Icon(icon),
+        trailing: const CupertinoListTileChevron(),
+        onTap: onTap,
+      );
+    } else {
+      return ListTile(
+        title: Text(title),
+        leading: Icon(icon),
+        trailing: const Icon(Icons.chevron_right),
+        onTap: onTap,
+      );
+    }
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+      ),
+      title: Image.asset(
+        'assets/title.webp',
+        height: 28,
+      ),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildListSection(List<Widget> children) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Column(
+        children: children,
+      ),
     );
   }
 
@@ -178,10 +295,17 @@ class _MenuState extends State<Menu> {
   }
 
   void _navigateTo(BuildContext context, Widget page) {
-    Navigator.push(
-      context,
-      CupertinoPageRoute(builder: (context) => page),
-    );
+    if (isIOS) {
+      Navigator.push(
+        context,
+        CupertinoPageRoute(builder: (context) => page),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => page),
+      );
+    }
   }
 
   void _navigateToPushNotificationSettings() async {

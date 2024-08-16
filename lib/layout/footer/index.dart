73a70f51485290cfe1c2bbfc0461a06d6702e47f
Arata <aratadayoon@yahoo.co.jp>
2024-08-16 23:09:12 +0900

    #12 add: footer_menu

--- lib/layout/footer/index.dart
+++ lib/layout/footer/index.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+bool isAndroid = Platform.isAndroid;
+bool isIOS = Platform.isIOS;
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -12,19 +17,44 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabBar(
-      items: const [
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.compass), label: 'ホーム'),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.search), label: '検索'),
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.square_grid_2x2), label: 'メニュー'),
-      ],
-      currentIndex: currentIndex,
-      backgroundColor: CupertinoColors.white,
-      onTap: onTap,
-      activeColor: CupertinoColors.activeBlue,
-      inactiveColor: CupertinoColors.inactiveGray,
-    );
+    if (isIOS) {
+      return CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.compass), label: 'ホーム'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search), label: '検索'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.square_grid_2x2), label: 'メニュー'),
+        ],
+        currentIndex: currentIndex,
+        backgroundColor: CupertinoColors.white,
+        onTap: onTap,
+        activeColor: CupertinoColors.activeBlue,
+        inactiveColor: CupertinoColors.inactiveGray,
+      );
+    } else {
+      return BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '検索',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view),
+            label: 'メニュー',
+          ),
+        ],
+        currentIndex: currentIndex,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+      );
+    }
   }
 }
